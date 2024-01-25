9ec0b1b9c078d9751c89777b794c3fe038bacff2
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2024-01-25 16:39:35 +0100

    Update Instalador.ps1

--- 1.Linux in Windows 11/Instalador.ps1
+++ 1.Linux in Windows 11/Instalador.ps1
@@ -1,6 +1,7 @@
 ﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl.exe --update
 wsl --set-default-version 2
 wsl --install -d Ubuntu-22.04
 Read-Host -Prompt "Finalizado, pulse enter para salir"
