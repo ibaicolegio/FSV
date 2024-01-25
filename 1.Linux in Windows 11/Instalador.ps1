728983d2bf6d91bed64eb8dcac763408d0a7465f
ibaicolegio <ibaicolegio@gmail.com>
2024-01-25 16:35:31 +0100

    Update Instalador.ps1

--- 1.Linux in Windows 11/Instalador.ps1
+++ 1.Linux in Windows 11/Instalador.ps1
@@ -1,3 +1,5 @@
-﻿Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 wsl --install
+Read-Host -Prompt "Finalizado, pulse enter para salir"
