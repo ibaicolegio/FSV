ab02bebaa77c7b56db4ee44e174864f85a1d363b
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2024-01-26 12:50:49 +0100

    Update instalador.sh

--- 2.fsoco/instalador.sh
+++ 2.fsoco/instalador.sh
@@ -1,4 +1,2 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 bash Anaconda3-2022.05-Linux-x86_64.sh
-sudo su
-apt-get update && apt-get install ffmpeg libsm6 libxext6 -y
